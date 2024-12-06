f64e0077cca9135532e5a005a808e021534d6e3e
TassiaBarbosa <tassiabarbosa@souunisuam.com.br>
2024-12-06 17:00:28 -0300

    Update config.page.ts

--- app/config/config.page.ts
+++ app/config/config.page.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../services/config.service';
 
@@ -8,92 +7,29 @@ import { ConfigService } from '../services/config.service';
   styleUrls: ['./config.page.scss'],
 })
 export class ConfigPage implements OnInit {
+  fontType = 'default';
+  darkMode = false;
+  fontSize = 16;
 
-  constructor(private configService: ConfigService) { }
+  constructor(private configService: ConfigService) {}
 
-  ngOnInit() {
-    const config = this.configService.getConfig();
-    if (config) {
-      this.currentFont = config.font || 'font-default';
-      this.currentFontSizeClass = config.fontSize || 'size-default';
-      this.currentContrastClass = config.contrast || 'contrast-default';
-    }
-  }
-  
-  //método para mudar a fonte 
-  public currentFont: string = 'font-default';
-  changeFont(fontClass: string) {
-    this.currentFont = fontClass;
+  async ngOnInit() {
+    await this.loadConfig();
   }
 
-  //método para mudar o tamanho da fonte
-  public currentFontSizeClass: string = 'size-default';  
-  changeFontSize(fontSizeClass: string) { 
-    this.currentFontSizeClass = fontSizeClass; 
+  async loadConfig() {
+    this.fontType = (await this.configService.getConfig('fontType')) || 'default';
+    this.darkMode = (await this.configService.getConfig('darkMode')) || false;
+    this.fontSize = (await this.configService.getConfig('fontSize')) || 16;
   }
 
-  //método para mudar o contraste da fonte
-  public currentContrastClass: string = 'contrast-default'; 
-  changeContrast(contrastClass: string) { 
-    this.currentContrastClass = contrastClass; 
-  }
-  //método de salvar configurações
-  salvarConfig() {
-    this.configService.setConfig(
-      this.currentFont, this.currentFontSizeClass, this.currentContrastClass
-    ) 
-  }
+  async updateConfig() {
+    await this.configService.setConfig('fontType', this.fontType);
+    await this.configService.setConfig('darkMode', this.darkMode);
+    await this.configService.setConfig('fontSize', this.fontSize);
 
+    document.body.style.setProperty('--font-type', this.fontType);
+    document.body.style.setProperty('--font-size', `${this.fontSize}px`);
+    document.body.classList.toggle('dark-mode', this.darkMode);
+  }
 }
-
-
-
-
-
-
-
-
-
-/**
-import { Component, OnInit } from '@angular/core';
-import { ConfigService } from '../services/config.service';
-
-@Component({
-  selector: 'app-config',
-  templateUrl: './config.page.html',
-  styleUrls: ['./config.page.scss'],
-})
-export class ConfigPage implements OnInit {
-
-  constructor(private configService: ConfigService) { }
-
-  ngOnInit() {
-  }
-  
-  //método para mudar a fonte 
-  public currentFont: string = 'font-default';
-  changeFont(fontClass: string) {
-    this.currentFont = fontClass;
-  }
-
-  //método para mudar o tamanho da fonte
-  public currentFontSizeClass: string = 'size-default';  
-  changeFontSize(fontSizeClass: string) { 
-    this.currentFontSizeClass = fontSizeClass; 
-  }
-
-  //método para mudar o contraste da fonte
-  public currentContrastClass: string = 'contrast-defaut'; 
-  changeContrast(contrastClass: string) { 
-    this.currentContrastClass = contrastClass; 
-  }
-  //método de salvar configurações
-  salvarConfig() {
-    this.configService.setConfig(
-      this.currentFont, this.currentFontSizeClass, this.currentContrastClass
-    ) 
-  }
-
-}
- */
-
